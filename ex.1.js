9bb2e37d586e601d1d8e3ee248d01857e23f4838
Abdiel Melendez <abdielhmelendez@gmail.com>
2015-10-31 16:20:41 -0700

    Update ex.1.js

--- ex.1.js
+++ ex.1.js
@@ -1,5 +1,8 @@
+//A function for finding the largest number in an array
+// create an array called results and set it to null
+
 function largestOfFour(arr) {
-  var results = [];
+  var results = []; 
   for (var n = 0; n < arr.length; n++) {
     var largestNumber = 0;
     for (var sb = 0; sb < arr[n].length; sb++) {
