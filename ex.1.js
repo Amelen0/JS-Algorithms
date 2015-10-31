aacc818b29eee5fafd23d8efd1f14ecb02c9957d
AbdielMelendez/ <abdielhmelendez@gmail.com>
2015-10-31 16:18:08 -0700

    Finished algorithm

--- ex.1.js
+++ ex.1.js
@@ -1,4 +1,17 @@
 function largestOfFour(arr) {
-  return arr;
-  console.log("Hello");
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = 0;
+    for (var sb = 0; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
+  }
+
+  return results;
 }
+
+
