27e6b018f422395a897c8d1ac30ce1ea586d9dd4
AbdielMelendez/ <abdielhmelendez@gmail.com>
2015-12-15 12:14:00 -0800

    New script

---
+++ DiffTwoARR.js
@@ -0,0 +1,8 @@
+/* This is a script that checks if two different arrays carry the same number*/
+
+
+function diff(arr1,arr2){
+ var arrNew = arr1.conact(arr2);
+ 
+function check(item){
+ if arr1.indexOf(item) 
